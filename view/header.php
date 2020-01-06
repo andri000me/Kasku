aa673ec2e175de0a9a7f75070bc6d97a1238aa3a
Farhan <faarhan7@gmail.com>
2020-01-06 10:32:56 +0700

    header edit

--- view/header.php
+++ view/header.php
@@ -14,7 +14,7 @@
   	</head>
   	<body>
 		<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-		  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+		  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Sipuas</a>
 		  <ul class="navbar-nav px-3">
 		    <li class="nav-item">
 		      <a class="nav-link" href="logout.php">Keluar</a>
